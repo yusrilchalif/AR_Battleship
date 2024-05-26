3b229ea2189b2ead202965ed034c0b482b07924e
Mayvan Gandhy <mayvangandhy357@gmail.com>
2024-05-26 19:27:07 +0700

    Update assets. and setup mekanik tutorial

--- Assets/Script/ImageTutorial.cs
+++ Assets/Script/ImageTutorial.cs
@@ -2,30 +2,57 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class ImageTutorial : MonoBehaviour
 {
-    public List<Sprite> spriteList; // Your list of sprites
-    public Image image; // The image you want to change
-    private int currentIndex = 0; // The current index of the sprite
+    public List<Sprite> spriteList;
+    public List<string> textList;
+    public Image image;
+    public TMP_Text text;
+    public int currentIndex = 0;
+
+    // Reference to the buttons
+    public Button previousButton;
+    public Button nextButton;
+
+    public void ResetIndex()
+    {
+        currentIndex = 0;
+    }
+
+    private void Start()
+    {
+        // Initialize button click listeners
+        previousButton.onClick.AddListener(PreviousSprite);
+        nextButton.onClick.AddListener(NextSprite);
+        currentIndex = 0;
+        UpdateImageAndText(0);
+    }
 
     public void NextSprite()
     {
-        // Increase the index and wrap around to the start if necessary
-        currentIndex = (currentIndex + 1) % spriteList.Count;
-        // Change the sprite
-        image.sprite = spriteList[currentIndex];
+        currentIndex += 1;
+        if (currentIndex >= spriteList.Count)
+        {
+            currentIndex = 0; // Kembali ke indeks pertama jika mencapai akhir daftar
+        }
+        UpdateImageAndText(currentIndex);
     }
 
     public void PreviousSprite()
     {
-        // Decrease the index and wrap around to the end if necessary
-        currentIndex--;
+        currentIndex -= 1;
         if (currentIndex < 0)
         {
-            currentIndex = spriteList.Count - 1;
+            currentIndex = spriteList.Count - 1; // Kembali ke indeks terakhir jika mencapai awal daftar
         }
-        // Change the sprite
-        image.sprite = spriteList[currentIndex];
+        UpdateImageAndText(currentIndex);
+    }
+
+    private void UpdateImageAndText(int index)
+    {
+        image.sprite = spriteList[index];
+        text.text = textList[index];
     }
 }
